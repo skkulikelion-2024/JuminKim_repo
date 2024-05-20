773ad45c99847447edf5ad5adf88673dd2125379
juminzoomout <juminzoomout@gmail.com>
2024-05-20 13:04:31 +0900

    seventh assignment-ver2(coin tracker)

--- 노마드코더 React/react-for-beginners/src/App.js
+++ 노마드코더 React/react-for-beginners/src/App.js
@@ -1,36 +1,51 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState([]);
+  const [value, setValue] = useState();
+  const [cryp, setCryp] = useState(0);
+  const onChange = (event) => {
+    setMoney(event.target.value);
+    setCryp(() => event.target.value / value);
   };
-  console.log(toDos);
+  const changeV = (event) => {
+    setValue(event.target.value);
+  };
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      <div>
         <input
+          value={money}
           onChange={onChange}
-          value={toDo}
           type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+          placeholder="how much do you have?"
+        ></input>
+        <hr />
+      </div>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select value={value} onChange={changeV}>
+          <option selected="selected">choose one</option>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <h4>You can buy {cryp} coins</h4>
     </div>
   );
 }
